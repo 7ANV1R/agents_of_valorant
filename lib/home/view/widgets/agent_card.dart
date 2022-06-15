2528d6f6b98bacdece4bd488a7bd66cee7240c81
7anv1r <ibnmizan7878@gmail.com>
2022-06-15 13:28:34 +0600

    updated card design

--- lib/home/view/widgets/agent_card.dart
+++ lib/home/view/widgets/agent_card.dart
@@ -1,4 +1,5 @@
 import 'package:agents_of_valorant/data/model/agent_model.dart';
+import 'package:agents_of_valorant/home/view/widgets/casing_extension.dart';
 import 'package:agents_of_valorant/widgets/loader_wave.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
@@ -54,18 +55,69 @@ class AgentCard extends StatelessWidget {
             ),
           ),
           Positioned(
-            top: size.height * 0.7,
-            // left: 40,
-            right: 100,
+            // top: size.height * 0.7,
+            bottom: size.height * 0.13,
+            left: 40,
+            // right: 100,
             child: Opacity(
               opacity: 1.0 - factorChange!,
-              child: Text(
-                agent!.name.toUpperCase(),
-                style: GoogleFonts.roboto(
-                  fontSize: 48,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Transform.scale(
+                    scaleX: -1,
+                    scaleY: -1,
+                    child: Text(
+                      '"',
+                      style: GoogleFonts.bizUDGothic(
+                        fontSize: 96,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Row(
+                    children: [
+                      const SizedBox(
+                        width: 32,
+                      ),
+                      SizedBox(
+                        width: size.width * 0.75,
+                        child: Text(
+                          agent!.selectionVoice,
+                          softWrap: true,
+                          style: GoogleFonts.roboto(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 64,
+                  ),
+                  Text(
+                    agent!.name.toUpperCase(),
+                    style: GoogleFonts.roboto(
+                      fontSize: 64,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text(
+                    agent!.role.toCapitalized(),
+                    style: GoogleFonts.roboto(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
